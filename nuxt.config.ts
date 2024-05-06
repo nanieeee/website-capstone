2b38566514237e1ecf5707fdfe5da9c8cf58f7ca
Faranie Erisca <faranie.erisca21@northwestu.edu>
2024-05-06 10:25:56 -0700

    modified nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,3 @@ export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss']
 })
 
-function defineNuxtConfig(arg0: { devtools: { enabled: boolean; }; modules: string[]; }) {
-  throw new Error("Function not implemented.");
-}
-
